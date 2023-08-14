a53360c2f9b67fa6857a779d5d776d7ce9bef8ab
Pramesh <Pramesh@DESKTOP-2GMSBR5>
2023-08-14 11:03:37 +0545

    Add project files.

---
+++ Spotify/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Spotify
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Connection = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.Execution = new System.Windows.Forms.Button();
+            this.UploadFiles = new System.Windows.Forms.Button();
+            this.SelectAll = new System.Windows.Forms.Button();
+            this.DeleteAll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Connection
+            // 
+            this.Connection.Location = new System.Drawing.Point(587, 173);
+            this.Connection.Name = "Connection";
+            this.Connection.Size = new System.Drawing.Size(75, 23);
+            this.Connection.TabIndex = 0;
+            this.Connection.Text = "Connection";
+            this.Connection.UseVisualStyleBackColor = true;
+            this.Connection.Click += new System.EventHandler(this.Connection_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(26, 90);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(379, 349);
+            this.checkedListBox1.TabIndex = 1;
+            // 
+            // Execution
+            // 
+            this.Execution.Location = new System.Drawing.Point(587, 111);
+            this.Execution.Name = "Execution";
+            this.Execution.Size = new System.Drawing.Size(75, 23);
+            this.Execution.TabIndex = 2;
+            this.Execution.Text = "Execute";
+            this.Execution.UseVisualStyleBackColor = true;
+            this.Execution.Click += new System.EventHandler(this.Execution_Click);
+            // 
+            // UploadFiles
+            // 
+            this.UploadFiles.Location = new System.Drawing.Point(587, 82);
+            this.UploadFiles.Name = "UploadFiles";
+            this.UploadFiles.Size = new System.Drawing.Size(75, 23);
+            this.UploadFiles.TabIndex = 4;
+            this.UploadFiles.Text = "Upload Files";
+            this.UploadFiles.UseVisualStyleBackColor = true;
+            this.UploadFiles.Click += new System.EventHandler(this.UploadFiles_Click);
+            // 
+            // SelectAll
+            // 
+            this.SelectAll.Location = new System.Drawing.Point(26, 50);
+            this.SelectAll.Name = "SelectAll";
+            this.SelectAll.Size = new System.Drawing.Size(75, 23);
+            this.SelectAll.TabIndex = 5;
+            this.SelectAll.Text = "Select All";
+            this.SelectAll.UseVisualStyleBackColor = true;
+            this.SelectAll.Click += new System.EventHandler(this.SelectAll_Click);
+            // 
+            // DeleteAll
+            // 
+            this.DeleteAll.Location = new System.Drawing.Point(133, 49);
+            this.DeleteAll.Name = "DeleteAll";
+            this.DeleteAll.Size = new System.Drawing.Size(75, 23);
+            this.DeleteAll.TabIndex = 6;
+            this.DeleteAll.Text = "Delete All";
+            this.DeleteAll.UseVisualStyleBackColor = true;
+            this.DeleteAll.Click += new System.EventHandler(this.DeleteAll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DeleteAll);
+            this.Controls.Add(this.SelectAll);
+            this.Controls.Add(this.UploadFiles);
+            this.Controls.Add(this.Execution);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.Connection);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Connection;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button Execution;
+        private System.Windows.Forms.Button UploadFiles;
+        private System.Windows.Forms.Button SelectAll;
+        private System.Windows.Forms.Button DeleteAll;
+    }
+}
+
---
+++ Spotify/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spotify
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        string conn = "Data Source=.;Initial Catalog=Spotify;Integrated Security=True";
+
+        private void Connection_Click(object sender, EventArgs e)
+        {
+
+            TestConnection();
+
+        }
+
+        private void Execution_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (selectAll() == true)
+
+                // selectAll().GetType== true;    
+                {
+                    string[] files = openFileDialog.FileNames;
+
+                    foreach (string file in files)
+                    {
+                        string sqlContent = File.ReadAllText(file);
+
+                        using (SqlConnection connection = new SqlConnection(conn))
+                        {
+                            connection.Open();
+                            SqlCommand command = connection.CreateCommand();
+                            command.CommandText = sqlContent;
+                            command.ExecuteNonQuery();
+                            connection.Close();
+
+                        }
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("You haven't selected the files  ");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+
+
+
+        }
+
+
+
+        public bool TestConnection()
+        {
+
+            try
+            {
+
+                SqlConnection connection = new SqlConnection(conn);
+                connection.Open();
+                MessageBox.Show("Database Connection Succesful");
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            return false;
+        }
+
+
+
+        private void UploadFiles_Click(object sender, EventArgs e)
+        {
+
+
+            openFileDialog.Multiselect = true;
+            this.openFileDialog.Filter = "SQL | *.sql";
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string[] files = openFileDialog.FileNames;
+
+                foreach (string file in files)
+                {
+
+                    string filename = Path.Combine("Files", Path.GetFileName(file));
+                    UpLoadFilesToDatabase(Path.GetFileName(file), filename);
+                    checkedListBox1.Items.Add(file);
+                }
+            }
+
+        }
+
+        private void SelectAll_Click(object sender, EventArgs e)
+        {
+
+            selectAll();
+        }
+
+        public bool selectAll()
+        {
+            for (int i = 0; i< checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, true);
+            }
+            return true;
+        }
+
+        private void DeleteAll_Click(object sender, EventArgs e)
+        {
+
+            string[] files = openFileDialog.FileNames;
+
+            foreach (string file in files)
+            {
+                string filename = Path.Combine("files", Path.GetFileName(file));
+                DeleteFileFromDatabase(Path.GetFileName(file), filename);
+                checkedListBox1.Items.Remove(file);
+            }
+
+
+        }
+        public void UpLoadFilesToDatabase(string filename, string filepath)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(conn))
+            {
+                sqlConnection.Open();
+                string query = "Insert into Files (filename, filepath) values (@filename,@filepath)";
+                using (SqlCommand com = new SqlCommand(query, sqlConnection))
+                {
+                    com.Parameters.AddWithValue("@filename", filename);
+                    com.Parameters.AddWithValue("@filepath", filepath);
+                    com.ExecuteNonQuery();
+                }
+
+            }
+        }
+
+
+
+
+        private void DeleteFileFromDatabase(string fileName, string filepath)
+        {
+            using (SqlConnection connection = new SqlConnection(conn))
+            {
+                connection.Open();
+
+                string query = "DELETE FROM Files";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@FileName", fileName);
+                    command.Parameters.AddWithValue("@Filepath", filepath);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+    }
+}
